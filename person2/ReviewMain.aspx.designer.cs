9b51f1cbcb66ebcd75c2c2a36a709c5728ffa097
ko02975157 <ko02975157@gmail.com>
2017-09-07 16:55:20 +0800

    新增ReviewMain

--- person2/ReviewMain.aspx.designer.cs
+++ person2/ReviewMain.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace person2 {
     
     
-    public partial class reviewdetail {
+    public partial class ReviewMain {
         
         /// <summary>
         /// Panel1 控制項。
@@ -31,13 +31,13 @@ namespace person2 {
         protected global::Telerik.Web.UI.RadMediaPlayer RadMediaPlayer1;
         
         /// <summary>
-        /// btn1 控制項。
+        /// Button1 控制項。
         /// </summary>
         /// <remarks>
         /// 自動產生的欄位。
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btn1;
+        protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
         /// Panel2 控制項。
@@ -58,13 +58,13 @@ namespace person2 {
         protected global::Telerik.Web.UI.RadMediaPlayer RadMediaPlayer2;
         
         /// <summary>
-        /// btn2 控制項。
+        /// Button2 控制項。
         /// </summary>
         /// <remarks>
         /// 自動產生的欄位。
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btn2;
+        protected global::System.Web.UI.WebControls.Button Button2;
         
         /// <summary>
         /// Panel3 控制項。
@@ -94,42 +94,6 @@ namespace person2 {
         protected global::System.Web.UI.WebControls.Label lbl2;
         
         /// <summary>
-        /// txt1 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txt1;
-        
-        /// <summary>
-        /// ddl1 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddl1;
-        
-        /// <summary>
-        /// txt2 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txt2;
-        
-        /// <summary>
-        /// ddl2 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddl2;
-        
-        /// <summary>
         /// lbl3 控制項。
         /// </summary>
         /// <remarks>
@@ -148,12 +112,48 @@ namespace person2 {
         protected global::System.Web.UI.WebControls.Label lbl4;
         
         /// <summary>
-        /// btn3 控制項。
+        /// txt1 控制項。
         /// </summary>
         /// <remarks>
         /// 自動產生的欄位。
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btn3;
+        protected global::System.Web.UI.WebControls.TextBox txt1;
+        
+        /// <summary>
+        /// lbl5 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl5;
+        
+        /// <summary>
+        /// txt2 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt2;
+        
+        /// <summary>
+        /// ddl1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl1;
+        
+        /// <summary>
+        /// lbl6 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl6;
     }
 }
