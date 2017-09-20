c4a9cbd6c13d635a692bb64452d052aeddd9c350
ko02975157 <ko02975157@gmail.com>
2017-09-20 15:45:05 +0800

    修改ReviewDetail中的版面排版

--- person2/MediaUpload.aspx.cs
+++ person2/MediaUpload.aspx.cs
@@ -106,7 +106,77 @@ namespace person2
 				int result = cmd.ExecuteNonQuery();
 				if (result == 1)
 				{
-					Response.Redirect("MediaUpload.aspx");
+                    try
+                    {
+                        using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["OTTConnectionString"].ConnectionString))
+                        {
+
+                            conn.Open();
+                            string sqlStatement = "Select PID From Person Where PRoleType=@PRoleType";
+                            SqlCommand cmd = new SqlCommand(sqlStatement, conn);
+                            cmd.Parameters.AddWithValue("@PRoleType", UserName);
+
+                            using (SqlDataReader rd = cmd.ExecuteReader())
+                            {
+                                if (rd.Read())
+                                {
+                                    pId = (int)rd["PID"];
+
+                                    string strPRoleType = rd["PRoleType"].ToString();
+                                    if (int.TryParse(strPRoleType, out pRoleType) == false)
+                                    {
+                                        this.lblshow.Text = "登入成功，但資料有錯誤! pRoleType的值為" + strPRoleType;
+                                        return;
+                                    }
+
+                                    string strPSuper = rd["PSuper"].ToString();
+                                    if (int.TryParse(strPSuper, out pSuper) == false)
+                                    {
+                                        this.lblshow.Text = "登入成功，但資料有錯誤! pSuper的值為" + strPSuper;
+                                        return;
+                                    }
+                                    pCode = rd["PCode"].ToString();
+                                    pName = rd["PName"].ToString();
+                                    pEmail = rd["PEmail"].ToString();
+                                    pPassword = rd["PPassword"].ToString();
+                                    pCountry = rd["PCountry"].ToString();
+                                    string strPBirthDate = rd["PBirthDate"].ToString();
+                                    DateTime birthday = DateTime.Parse(strPBirthDate);
+
+
+
+                                    this.lblshow.Text = "登入成功";
+
+                                    Session["PID"] = pId;
+                                    Session["PRoleType"] = pRoleType;
+                                    Session["PSuper"] = pSuper;
+                                    Session["PCode"] = pCode;
+                                    Session["PName"] = pName;
+                                    Session["PEmail"] = pEmail;
+                                    Session["PPassword"] = pPassword;
+                                    Session["PCountry"] = pCountry;
+                                    Session["PBirthDate"] = birthday;
+                                    // Server.TransferRequest("MemberInformationChange.aspx"); 無法session
+                                    Response.Redirect("Default.aspx");   //可以session
+                                }
+                                else
+                                {
+                                    this.lblshow.Text = "登入失敗";
+                                }
+                            }
+
+                        }
+                        this.lblshow.Text = "資料連線成功";
+
+                    }
+
+                    catch (Exception)
+                    {
+                        this.lblshow.Text = "資料連線錯誤";
+                    }
+                }
+
+                Response.Redirect("MediaUpload.aspx");
 				}
 			}
 		}
